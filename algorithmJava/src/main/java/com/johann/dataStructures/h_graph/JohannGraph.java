5cd90fc11928545f4855c3d0969f2bdcc8f34b56
Johann <yhzhaohy@hotmail.com>
2022-04-20 18:02:09 +0800

    graph DFS,BFS简述

--- algorithmJava/src/main/java/com/johann/dataStructures/h_graph/JohannGraph.java
+++ algorithmJava/src/main/java/com/johann/dataStructures/h_graph/JohannGraph.java
@@ -115,6 +115,100 @@ public class JohannGraph {
         return -1;
     }
 
+    /**
+     * 打印当前顶点信息
+     * @param index
+     */
+    public void showVertex(int index){
+        System.out.println(vertexArray[index].data);
+    }
+
+    /**
+     * 搜索完毕，重置各个顶点的“访问情况”
+     */
+    public void resetVertexVisited(){
+        for (int i=0;i < elementCount;i++){
+            vertexArray[i].setVisited(false);
+        }
+    }
+
+    /**
+     * 深度优先搜索
+     * 规则1：如果可能，访问一个邻接的未访问顶点，标记它，并将它放入栈中。
+     * 规则2：当不能执行规则 1 时，如果栈不为空，就从栈中弹出一个顶点。
+     * 规则3：如果栈为空，不能执行规则 1 和规则 2 时，就完成了整个搜索过程。
+     */
+    public void depthFirstSearch(){
+        //先访问第一个顶点，将该顶点的“访问情况”更新为 true
+        vertexArray[0].setVisited(true);
+        //展示当前顶点
+        showVertex(0);
+        //int currentIndex = 0;
+        //将当前顶节点压入栈内
+        depthSearchStack.push(0);
+
+        //如果深度搜索栈不为空，则循环处理
+        while(!depthSearchStack.isEmpty()){
+            // 使用 depthSearchStack.peak() 减少一个变量 currentIndex
+            //int adjacentIndex = findNonVisiteAdjacentVertex(currentIndex);
+            //获取栈顶顶点，还没有被访问过的邻近顶点
+            int adjacentIndex = findNonVisiteAdjacentVertex(depthSearchStack.peak());
+            //如果栈顶顶点有邻近顶点
+            if (adjacentIndex != -1){
+                //将这个邻近顶点的“访问情况”更新为 true
+                vertexArray[adjacentIndex].setVisited(true);
+                //展示这个邻近顶点
+                showVertex(adjacentIndex);
+                //将这个邻近顶点压入栈内
+                depthSearchStack.push(adjacentIndex);
+                //currentIndex = adjacentIndex;
+            //如果栈顶顶点有邻近顶点，栈顶顶点出栈
+            }else{
+                depthSearchStack.pop();
+                //currentIndex = depthSearchStack.peak();
+            }
+        }
+
+        //搜索完毕，重置各个顶点的“访问情况”
+        resetVertexVisited();
+    }
+
+
+    /**
+     * 广度优先搜索
+     * 规则1：访问下一个未访问的邻接点（如果存在），这个顶点必须是当前顶点的邻接点，标记它，并把它插入到队列中。
+     * 规则2：如果已经没有未访问的邻接点而不能执行规则 1 时，那么从队列列头取出一个顶点（如果存在），并使其成为当前顶点。
+     * 规则3：如果队列为空，不能执行规则 2，则搜索结束。
+     */
+    public void breadthFirstSearch(){
+        vertexArray[0].setVisited(true);
+        showVertex(0);
+        //将当前顶点插入到队列中
+        breadthSearchQueue.insert(0);
+
+        //如果队列不为空，循环处理
+        while(!breadthSearchQueue.isEmpty()){
+            //循环检索队首顶点，获取该顶点还未被访问的邻近顶点，直至没有“还未被访问的邻近顶点”
+            int adjacentIndex = findNonVisiteAdjacentVertex((int)breadthSearchQueue.peakFront());
+            //如果队首顶点的邻近顶点不为空
+            if (adjacentIndex != -1){
+                //将邻近顶点设置为“已访问”，并插入到队尾等待后续检索
+                vertexArray[adjacentIndex].setVisited(true);
+                showVertex(adjacentIndex);
+                breadthSearchQueue.insert(adjacentIndex);
+            //如果队首顶点的邻近顶点检索完毕，将队首顶点移除队列
+            }else{
+                breadthSearchQueue.remove();
+            }
+        }
+
+        //搜索完毕，重置各个顶点的“访问情况”
+        resetVertexVisited();
+    }
+
+
+
+
 
     public static void main(String[] args) {
         JohannGraph graph = new JohannGraph(8);
@@ -131,8 +225,8 @@ public class JohannGraph {
         graph.addEdge(0,3);//AD
         graph.addEdge(0,4);//AE
         graph.addEdge(1,2);//BC
-        graph.addEdge(1,7);//BH
-        graph.addEdge(2,5);//CF
+        //graph.addEdge(1,7);//BH
+        //graph.addEdge(2,5);//CF
         graph.addEdge(3,4);//DE
         graph.addEdge(3,6);//DG
         graph.addEdge(4,6);//EG
@@ -140,13 +234,18 @@ public class JohannGraph {
         graph.addEdge(5,7);//FH
         graph.addEdge(6,7);//GH
 
-        int adjacentIndex = graph.findNonVisiteAdjacentVertex(0);
-        while(adjacentIndex != -1){
-            System.out.println(graph.vertexArray[adjacentIndex].data);
-            graph.vertexArray[adjacentIndex].setVisited(true);
-            adjacentIndex = graph.findNonVisiteAdjacentVertex(0);
-        }
+//        int adjacentIndex = graph.findNonVisiteAdjacentVertex(0);
+//        while(adjacentIndex != -1){
+//            System.out.println(graph.vertexArray[adjacentIndex].data);
+//            graph.vertexArray[adjacentIndex].setVisited(true);
+//            adjacentIndex = graph.findNonVisiteAdjacentVertex(0);
+//        }
 
+        System.out.println("深度优先搜索结果：");
+        graph.depthFirstSearch();
+
+        System.out.println("广度优先搜索结果：");
+        graph.breadthFirstSearch();
     }
 
 
