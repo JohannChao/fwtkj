70930f4f69c5fd98a50fddcbf3916d7f5ee100ab
Johann <yhzhaohy@hotmail.com>
2022-11-04 00:22:24 +0800

    jzII003

--- algorithmLearning/src/main/java/com/johann/jianzhiOffer/[剑指 Offer II 003]前 n 个数字二进制中 1 的个数.java
+++ algorithmLearning/src/main/java/com/johann/jianzhiOffer/[剑指 Offer II 003]前 n 个数字二进制中 1 的个数.java
@@ -57,142 +57,124 @@ package com.johann.jianzhiOffer;
 class SolutionJianzhiII003 {
     public int[] countBits(int n) {
         /**
-         * 笨比方法
-         */
-//        int [] countArray = new int[n+1];
-//        while (n >= 0){
-//            countArray[n] = Integer.bitCount(n);
-//            n--;
-//        }
-//        return countArray;
-
-        /**
+         * 首先看一下 0--15 之间的二进制，包含的1 有什么规律。
+         * 二进制   包含1的个数
          * 0       0
-         * 01      1
+         * 1       1           2^1
+         *
          * 10      1
-         * 11      2        2^2
+         * 11      2           2^2
+         *
          * 1 00    1+0
          * 1 01    1+1
          * 1 10    1+1
-         * 1 11    1+2      2^3
-         * 1  000  1+0
-         * 1  001  1+1
-         * 1  010  1+1
-         * 1  011  1+1
-         * 1  100  1+(1+0)
-         * 1  101  1+(1+1)
-         * 1  110  1+(1+1)
-         * 1  111  1+(1+2)  2^4
+         * 1 11    1+2         2^3
+         *
+         * 1 000   1+0
+         * 1 001   1+1
+         * 1 010   1+1
+         * 1 011   1+1
+         * 1 100   1+(1+0)
+         * 1 101   1+(1+1)
+         * 1 110   1+(1+1)
+         * 1 111   1+(1+2)     2^4
+         *
+         * 规律就是：二进制每增加一位，就会在之前所有的二进制数前面添加一位，且是固定的 1。
+         * 所以，可以用循环的方式，得出之后的二进制数包含 1 的个数，循环的入口就是，前两个数字[0,1]
          */
-        int[] countArray = new int[n + 1];
-        while (n < 1 << 2) {
-            switch (n) {
-                case 0:
-                    countArray[n] = 0;
-                    break;
-                case 1:
-                case 2:
-                    countArray[n] = 1;
-                    break;
-                case 3:
-                    countArray[n] = 2;
-                    break;
-                default:
-                    ;
-            }
-            n++;
-        }
 
-        while (n >= 1 << 2 && n < 1 << 3) {
-            countArray[n] = 1 + countArray[n | ((1 << 2) - 1)];
-        }
-        while (n >= 1 << 3 && n < 1 << 4) {
-            countArray[n] = 1 + countArray[n | ((1 << 3) - 1)];
-        }
-
-        int moveCount = 2;
-        for (int i = 0; i <= n; i++) {
-            if (0 <= i && i < 4) {
-                switch (i) {
+        /**
+         * 第一步，先写一个循环的出口
+            int[] countArray = new int[n + 1];
+            while (n < 2) {
+                switch (n) {
                     case 0:
-                        countArray[i] = 0;
+                        countArray[n] = 0;
                         break;
                     case 1:
-                    case 2:
-                        countArray[i] = 1;
-                        break;
-                    case 3:
-                        countArray[i] = 2;
+                        countArray[n] = 1;
                         break;
                     default:
                         ;
                 }
+                n++;
+            }
+         * 第二步，总结其他的二进制的规律。
+         * 一个二进制数 N 中包含的 1 的个数，将这个二进制 N 的最高位由 1 变成 0，得到的一个二进制数 M。M中1的个数+1，就是 N 中1的个数。
+         * 当我们从0开始循环遍历的时候，这个N里面1的个数，肯定是已经求出来的。现在问题是，如何根据 M 得到 N。
+         * 举个例子：N = 1101,1100(8位)，想到 M 应该是 0101,1100。
+         * 1101,1100
+         * &
+         * 0111,1111((1<<8-1))
+         * =
+         * 0101,1100
+         * 即，M = N & ((1<<N的位数)-1)
+            while (n >= 1 << 2 && n < 1 << 3) {
+                countArray[n] = 1 + countArray[n & ((1 << 2) - 1)];
+            }
+            while (n >= 1 << 3 && n < 1 << 4) {
+                countArray[n] = 1 + countArray[n & ((1 << 3) - 1)];
+            }
+         */
+        int[] countArray = new int[n + 1];
+        //当前循环中 i 的位数
+        int moveCount = 1;
+        //从 0~n 循环遍历
+        for (int i = 0; i <= n; i++) {
+            //循环入口
+            if (i==0){
+                countArray[i] = 0;
+            }else if(i==1){
+                countArray[i] = 1;
             } else {
-                //移动位数
-
+                //确定循环 i 的二进制位数
+                if (i >= 1 << moveCount && i < 1 << (moveCount + 1)) {
+                    //当前二进制数中1的个数，等于这个二进制数砍掉最高位的另一个二进数中1的个数+1（听起来像是废话）
+                    countArray[i] = 1 + countArray[i & ((1 << moveCount) - 1)];
+                } else {
+                    //如果 i 的二进制位数发生变化，记录该位数的moveCount+1，别忘了再执行一遍数组赋值操作
+                    moveCount++;
+                    countArray[i] = 1 + countArray[i & ((1 << moveCount) - 1)];
+                }
             }
         }
+        return countArray;
 
-        return null;
+        /**
+         * MD，如果砍最低位，岂不是更简单！！！
+         */
     }
 
+    public int[] countBits2(int n) {
+        /**
+         * 笨比方法
+         */
+        int[] countArray = new int[n + 1];
+        while (n >= 0) {
+            countArray[n] = Integer.bitCount(n);
+            n--;
+        }
+        return countArray;
+    }
 
     public static void main(String[] args) {
-        //new SolutionJianzhiII003().countBits(10);
-
-        int m = Integer.parseInt("11110011", 2);
-        int n = m >> 1;
-        System.out.println(Integer.toBinaryString(n));
-
-
-//        int cap = 100000;
-//        int n = cap - 1;
-//
-//        byte[] bs = Integer.toBinaryString(n).getBytes();
-//
-//        String sn = Integer.toBinaryString(n);
-//        System.out.println("\n--- n>>1 ---");
-//        System.out.println(sn);
-//
-//        String s1 = Integer.toBinaryString(n >>> 1);
-//        System.out.println(s1);
-//        n |= n >>> 1;
-//        String sn1 = Integer.toBinaryString(n);
-//        System.out.println(sn1);
-//
-//        System.out.println("\n--- n>>2 ---");
-//        System.out.println(sn1);
-//        String s2 = Integer.toBinaryString(n >>> 2);
-//        System.out.println(s2);
-//        n |= n >>> 2;
-//        String sn2 = Integer.toBinaryString(n);
-//        System.out.println(sn2);
-//
-//        System.out.println("\n--- n>>4 ---");
-//        System.out.println(sn2);
-//        String s4 = Integer.toBinaryString(n >>> 4);
-//        System.out.println(s4);
-//        n |= n >>> 4;
-//        String sn4 = Integer.toBinaryString(n);
-//        System.out.println(sn4);
-//
-//        System.out.println("\n--- n>>8 ---");
-//        System.out.println(sn4);
-//        String s8 = Integer.toBinaryString(n >>> 8);
-//        System.out.println(s8);
-//        n |= n >>> 8;
-//        String sn8 = Integer.toBinaryString(n);
-//        System.out.println(sn8);
-//
-//        System.out.println("\n--- n>>16 ---");
-//        System.out.println(sn8);
-//        String s16 = Integer.toBinaryString(n >>> 16);
-//        System.out.println(s16);
-//        n |= n >>> 16;
-//        String sn16 = Integer.toBinaryString(n);
-//        System.out.println(sn16);
-//
-//        System.out.println(n);
+        int[] array1 = new SolutionJianzhiII003().countBits2(100000);
+        int[] array2 = new SolutionJianzhiII003().countBits(100000);
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] != array2[i]) {
+                System.out.println("array1[" + i + "] = " + array1[i]);
+                System.out.println("array2[" + i + "] = " + array2[i]);
+                return;
+            }
+        }
+        System.out.println("Bingo");
+//        int m = Integer.parseInt("11110011", 2);
+//        int m1 = (1<<7);
+//        System.out.println(Integer.toBinaryString(m1));
+//        int m2 = (1<<8);
+//        System.out.println(Integer.toBinaryString(m2));
+//        int n = m&((1<<7)-1);
+//        System.out.println(Integer.toBinaryString(n));
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
