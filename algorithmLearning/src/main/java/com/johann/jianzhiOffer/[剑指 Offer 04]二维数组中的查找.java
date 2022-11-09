2b7126bb8ee3c43ea654a7f10d9f26377782acf2
Johann <yhzhaohy@hotmail.com>
2022-11-09 17:51:52 +0800

    004005

---
+++ algorithmLearning/src/main/java/com/johann/jianzhiOffer/[剑指 Offer 04]二维数组中的查找.java
@@ -0,0 +1,104 @@
+package com.johann.jianzhiOffer;
+//在一个 n * m 的二维数组中，每一行都按照从左到右 非递减 的顺序排序，每一列都按照从上到下 非递减 的顺序排序。请完成一个高效的函数，输入这样的一个二
+//维数组和一个整数，判断数组中是否含有该整数。 
+//
+// 
+//
+// 示例: 
+//
+// 现有矩阵 matrix 如下： 
+//
+// 
+//[
+//  [1,   4,  7, 11, 15],
+//  [2,   5,  8, 12, 19],
+//  [3,   6,  9, 16, 22],
+//  [10, 13, 14, 17, 24],
+//  [18, 21, 23, 26, 30]
+//]
+// 
+//
+// 给定 target = 5，返回 true。 
+//
+// 给定 target = 20，返回 false。 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= n <= 1000 
+//
+// 0 <= m <= 1000 
+//
+// 
+//
+// 注意：本题与主站 240 题相同：https://leetcode-cn.com/problems/search-a-2d-matrix-ii/ 
+//
+// Related Topics 数组 二分查找 分治 矩阵 👍 818 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class SolutionJianzhi004 {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        //数据类型[][]数组名=new 数据类型[外维数组容量][内维数组容量]; [行数][列数]
+        //int[][] arr = new int[2][3];
+        int n,m;
+        if (matrix == null || (n = matrix.length)==0 || matrix[0]==null || (m = matrix[0].length)==0){
+            return false;
+        }
+        //从二维数组的左下角开始遍历
+        for(int i=n-1,j=0;i>=0&&j<m;){
+            int temp = matrix[i][j];
+            //如果左下角的数据大于目标值，则行索引向上走一个格
+            if (matrix[i][j] > target){
+                i--;
+            //如果左下角的数据小于目标值，则列索引向右走一个格
+            }else if(matrix[i][j] < target){
+                j++;
+            }else {
+                System.out.println("命中，位于 "+i+" 行, "+j+" 列");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        /**
+         * 1，定义方法一：数据类型[][]数组名=new 数据类型[外维数组容量][内维数组容量];
+         *     //定义一个一维数组，长度为 2 ，在这个一维数组中存放子数组，子数组的默认长度是 3
+         *     int[][] arr = new int[2][3];
+         *     int[][] arr = new int[2][];
+         *     //1.1，赋值方式一：
+         *     // 采用这种方式赋值时，二维数组的 [行索引][列索引] 不能超出定义的 [2][3]
+         *     //第1个内维数组
+         *     arr [0][0]=90;
+         *     arr [0][1]=80;
+         *     arr [0][2]=70;
+         *     //第2个内维数组
+         *     arr [1][0]=90;
+         *     arr [1][1]=90;
+         *     arr [1][2]=80;
+         *     //1.2，赋值方式二：
+         *     // 采用这种方式赋值时，一维数组的子数组的长度，可以重新指定，不再局限于定义时预设的 [3]
+         *     arr[0] = new int[]{0,1,2,3};
+         *     arr[1] = new int[]{10,11,12,13};
+         *
+         * 2，定义方法二：数据类型[][] 数组名 = {{},{}...};
+         *     int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+         *
+         * 3，定义方法三：数据类型[][] 数组名 = new 数据类型[][]{{},{}...};
+         *     int[][] matrix = new int[][]{{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+         */
+        int[][] matrix = new int[][]{{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        System.out.println(matrix.length+" * "+matrix[0].length);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println(new SolutionJianzhi004().findNumberIn2DArray(matrix,5));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ algorithmLearning/src/main/java/com/johann/jianzhiOffer/[剑指 Offer 06]从尾到头打印链表.java
@@ -0,0 +1,131 @@
+package com.johann.jianzhiOffer;
+//输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：head = [1,3,2]
+//输出：[2,3,1] 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 链表长度 <= 10000 
+//
+// Related Topics 栈 递归 链表 双指针 👍 350 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class SolutionJianzhi006 {
+    public int[] reversePrint(ListNode head) {
+        if (head == null) {
+            return new int[0];
+        }
+        Deque<Integer> deque = new ArrayDeque();
+        do {
+            deque.push(head.val);
+        } while ((head = head.next) != null);
+
+        int size = deque.size();
+        int[] ints = new int[size];
+        int i = 0;
+        while (i < size) {
+            //pop()如果队列为空，抛出异常NoSuchElementException；poll()队列为空，返回null
+            ints[i] = deque.pop();
+            i++;
+        }
+        return ints;
+    }
+
+    /**
+     * 递归实现
+     * @param head
+     * @return
+     */
+    public int[] reversePrintRecursion(ListNode head){
+        if (head == null) {
+            return new int[0];
+        }
+        ArrayList<Integer> list = new ArrayList();
+        recursion(head,list);
+        int[] ints = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ints[i] = list.get(i);
+        }
+        return ints;
+    }
+    public void recursion(ListNode head, ArrayList list){
+        //递归出口
+        if (head == null) {
+            return;
+        }
+        recursion(head.next, list);
+        list.add(head.val);
+    }
+
+    /**
+     * 在递归出口处，获取链表长度，创建数组。减少后续遍历
+     * https://leetcode.cn/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/solutions/97270/mian-shi-ti-06-cong-wei-dao-tou-da-yin-lian-biao-d/comments/596683
+     */
+    //待返回的数组
+    int[] result = null;
+    //链表长度，即数组长度。数组索引偏移量
+    int size = 0, index = 0;
+    public int[] reversePrintRecursion2(ListNode head){
+        if (head == null) {
+            return new int[0];
+        }
+        recursion2(head);
+        return result;
+    }
+
+    public void recursion2(ListNode head){
+        if (head == null) {
+            result = new int[size];
+            return;
+        }
+        //递归过程中，当返回上一圈时，从上一圈执行到的点位开始，继续按顺序执行接下来的代码
+        size++;
+        recursion2(head.next);
+        result[index] = head.val;
+        index++;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        node1.next = node2;
+        ListNode node3 = new ListNode(3);
+        node2.next = node3;
+        ListNode node4 = new ListNode(4);
+        node3.next = node4;
+        ListNode node5 = new ListNode(5);
+        node4.next = node5;
+        ListNode node6 = new ListNode(6);
+        node5.next = node6;
+        ListNode node7 = new ListNode(7);
+        node6.next = node7;
+        ListNode node8 = new ListNode(8);
+        node7.next = node8;
+
+        new SolutionJianzhi006().reversePrintRecursion2(node1);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
