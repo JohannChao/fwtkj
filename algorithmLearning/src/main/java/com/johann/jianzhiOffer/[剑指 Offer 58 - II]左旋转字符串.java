c4575906bd903e3090c473ab7b531254708966e1
Johann <yhzhaohy@hotmail.com>
2022-11-14 01:34:12 +0800

    058

---
+++ algorithmLearning/src/main/java/com/johann/jianzhiOffer/[剑指 Offer 58 - II]左旋转字符串.java
@@ -0,0 +1,42 @@
+package com.johann.jianzhiOffer;//字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数
+//将返回左旋转两位得到的结果"cdefgab"。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入: s = "abcdefg", k = 2
+//输出: "cdefgab"
+// 
+//
+// 示例 2： 
+//
+// 输入: s = "lrloseumgh", k = 6
+//输出: "umghlrlose"
+// 
+//
+// 
+//
+// 限制： 
+//
+// 
+// 1 <= k < s.length <= 10000 
+// 
+//
+// Related Topics 数学 双指针 字符串 👍 318 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class SolutionJianzhi058 {
+    public String reverseLeftWords(String s, int n) {
+        if (s.length() == 0 || n == 0){
+            return s;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = n; i < s.length()+n; i++) {
+            stringBuilder.append(i < s.length() ? s.charAt(i) : s.charAt(i-s.length()));
+        }
+        return stringBuilder.toString();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
