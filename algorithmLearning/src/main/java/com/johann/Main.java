0f1bdd2996225750b5d16c9a629dfcae6c7eebf2
Johann <yhzhaohy@hotmail.com>
2022-11-16 01:38:36 +0800

    mysql datetime function

--- algorithmLearning/src/main/java/com/johann/Main.java
+++ algorithmLearning/src/main/java/com/johann/Main.java
@@ -1,7 +1,46 @@
 package com.johann;
 
 public class Main {
+
+    public int search(int[] nums,int target){
+        int result = 0;
+        int min = 0,max = nums.length-1,index = 0;
+        while (min <= max){
+            index = (min+max)>>1;
+            if (nums[index] == target) {
+                break;
+            }
+            if (nums[index] > target){
+                max = index-1;
+            }else {
+                min = index+1;
+            }
+        }
+
+        if (nums[index] == target){
+            result++;
+            int p = index+1;
+            while (p < nums.length) {
+                if (nums[p] != target){
+                    break;
+                }
+                p++;
+                result++;
+            }
+            p = index-1;
+            while (p >= 0) {
+                if (nums[p] != target){
+                    break;
+                }
+                p--;
+                result++;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println((1>>1));
     }
 }
